05a1c0ec8d1c5421d00bde75cb36df90f1c0eb6f
Donovan Glover <donovan@dglover.co>
2023-08-29 19:29:14 -0400

    feat: Add base workspace logic
    This *works*, and is the start of a pretty good solution when it comes
    to handling workspaces.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 use clap::Parser;
 use cli::Cli;
-use hyprnome::log;
+use hyprland::data::Workspace;
+use hyprland::data::Workspaces;
+use hyprland::data::Monitors;
+use hyprland::dispatch::*;
+use hyprland::prelude::*;
+use hyprland::dispatch::WorkspaceIdentifierWithSpecial as WID;
+use hyprnome::get_id;
+// use hyprnome::log;
 
 mod cli;
 
 /// Main function in charge of hyprnome logic.
 ///
 /// Specific features are abstracted into lib to make things testable.
-fn main() {
-    let Cli {
-        allow_going_backwards,
-        ..
-    } = Cli::parse();
+fn main() -> hyprland::Result<()> {
+    let Cli { previous, .. } = Cli::parse();
+    let workspaces = Workspaces::get()?;
 
-    log(&format!("{allow_going_backwards}"));
+    let current_id = Workspace::get_active()?.id;
+    let monitor_ids: Vec<i32> = workspaces.clone().filter(|x| x.monitor == Monitors::get().unwrap().find(|x| x.focused).unwrap().name).map(|x| x.id).collect();
+    let occupied_ids: Vec<i32> = workspaces.map(|x| x.id).collect();
+
+    let new_id = get_id(current_id, monitor_ids, occupied_ids);
+    hyprland::dispatch!(Workspace, WID::Id(new_id))?;
+
+    if previous {
+        hyprland::dispatch!(Workspace, WID::RelativeMonitor(-1))?;
+    } else {
+        hyprland::dispatch!(Workspace, WID::RelativeMonitor(1))?;
+    }
+
+    Ok(())
 }
