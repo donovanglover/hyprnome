e2310c6ba9127101b7d501f552c271f3986d040f
Donovan Glover <donovan@dglover.co>
2023-08-29 19:30:19 -0400

    lib: Begin writing get_id function

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,16 @@ pub fn log(text: &str) {
         println!("{text}")
     }
 }
+
+/// Gets the id
+pub fn get_id(current_id: i32, monitor_ids: Vec<i32>, occupied_ids: Vec<i32>) -> i32 {
+    // let current_id = 500
+    // let occupied_ids = [495, 496, 498, 500, 502, 504, 505]
+    // let monitor_ids = [496, 500, 504]
+    // if current_id is_not_first_in(monitor_ids) -> RelativeMonitor -1
+    // else new_workspace(first_id_of(occupied_ids) - 1)
+    if monitor_ids[0] == current_id {
+        let id = occupied_ids[0] - 1;
+    }
+    0
+}
