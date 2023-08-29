828f58504d150028def27bdff1aee923e1ab1281
Donovan Glover <donovan@dglover.co>
2023-08-29 15:35:51 -0400

    feat: Add generated shell completions

---
+++ completions/hyprnome.bash
@@ -0,0 +1,38 @@
+_hyprnome() {
+    local i cur prev opts cmd
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    cmd=""
+    opts=""
+
+    for i in ${COMP_WORDS[@]}
+    do
+        case "${cmd},${i}" in
+            ",$1")
+                cmd="hyprnome"
+                ;;
+            *)
+                ;;
+        esac
+    done
+
+    case "${cmd}" in
+        hyprnome)
+            opts="-a -v -h -V --allow-going-backwards --verbose --help --version"
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+    esac
+}
+
+complete -F _hyprnome -o nosort -o bashdefault -o default hyprnome
---
+++ completions/hyprnome.fish
@@ -0,0 +1,4 @@
+complete -c hyprnome -s a -l allow-going-backwards -d 'Create empty workspaces when going backwards'
+complete -c hyprnome -s v -l verbose -d 'Show information about what hyprnome is doing'
+complete -c hyprnome -s h -l help -d 'Print help (see more with \'--help\')'
+complete -c hyprnome -s V -l version -d 'Print version'
