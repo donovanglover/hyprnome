bc4f2e24eb85a025269cc94c12c274405208e3e3
Donovan Glover <donovan@dglover.co>
2023-08-30 10:08:46 -0400

    refactor: Simplify main function

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,13 @@ mod cli;
 /// Main function in charge of hyprnome logic.
 ///
 /// Specific features are abstracted into lib to make things testable.
-fn main() {
+fn main() -> hyprland::Result<()> {
     let Cli { _move, .. } = Cli::parse();
+    let id = WorkspaceIdentifierWithSpecial::Id(get_id());
 
     if _move {
-        hyprland::dispatch!(MoveToWorkspace, WorkspaceIdentifierWithSpecial::Id(get_id()), None).unwrap();
+        hyprland::dispatch!(MoveToWorkspace, id, None)
     } else {
-        hyprland::dispatch!(Workspace, WorkspaceIdentifierWithSpecial::Id(get_id())).unwrap();
+        hyprland::dispatch!(Workspace, id)
     }
 }
