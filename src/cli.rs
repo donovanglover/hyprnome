393287d1431bcf547b76d768966d643ccf9a9280
Donovan Glover <donovan@dglover.co>
2023-08-30 14:04:59 -0400

    chore: Improve long about

--- src/cli.rs
+++ src/cli.rs
@@ -3,8 +3,19 @@ use clap::Parser;
 const LONG_ABOUT: &str = "
 hyprnome is a program that enables you to achieve GNOME-like workspace switching in Hyprland.
 
-Unlike Hyprland's r+1/r-1, empty workspaces are not shown to the user when the user navigates
-between them.
+Unlike Hyprland's r+1/r-1, empty workspaces between two occupied workspaces are skipped.
+
+Unlike Hyprland's m+1/m-1, new workspaces are created instead of wrapping to existing ones.
+
+Example hyprland.conf:
+
+bind = $SUPER, 1, exec, hyprnome --previous
+
+bind = $SUPER, 2, exec, hyprnome
+
+bind = $SUPER_SHIFT, 1, exec, hyprnome --previous --move
+
+bind = $SUPER_SHIFT, 2, exec, hyprnome --move
 ";
 
 #[derive(Parser)]
