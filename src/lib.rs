1d78f4ba971625010224f79d4406321214cf94c6
Donovan Glover <donovan@dglover.co>
2023-08-30 10:18:50 -0400

    chore: Run cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
 use cli::Cli;
+use hyprland::data::Monitors;
 use hyprland::data::Workspace;
 use hyprland::data::Workspaces;
-use hyprland::data::Monitors;
 use hyprland::prelude::*;
 
 mod cli;
@@ -20,13 +20,18 @@ pub fn log(text: &str) {
 pub struct WorkspaceState {
     pub current_id: i32,
     pub monitor_ids: Vec<i32>,
-    pub occupied_ids: Vec<i32>
+    pub occupied_ids: Vec<i32>,
 }
 
 impl WorkspaceState {
     pub fn new() -> WorkspaceState {
         let workspaces = Workspaces::get().unwrap();
-        let mut monitor_ids: Vec<i32> = workspaces.clone().filter(|x| x.monitor == Monitors::get().unwrap().find(|x| x.focused).unwrap().name).map(|x| x.id).filter(|x| x > &0).collect();
+        let mut monitor_ids: Vec<i32> = workspaces
+            .clone()
+            .filter(|x| x.monitor == Monitors::get().unwrap().find(|x| x.focused).unwrap().name)
+            .map(|x| x.id)
+            .filter(|x| x > &0)
+            .collect();
         let mut occupied_ids: Vec<i32> = workspaces.map(|x| x.id).filter(|x| x > &0).collect();
 
         monitor_ids.sort();
@@ -51,7 +56,11 @@ impl WorkspaceState {
 /// 1) Returns the workspace id before the first occupied workspace (or 1)
 /// 2) Otherwise, since there are workspaces before on the same monitor, select the one before.
 pub fn get_previous_id(state: WorkspaceState, no_empty_before: bool) -> i32 {
-    let WorkspaceState { current_id, monitor_ids, occupied_ids } = state;
+    let WorkspaceState {
+        current_id,
+        monitor_ids,
+        occupied_ids,
+    } = state;
 
     if monitor_ids[0] == current_id {
         if occupied_ids[0] == 1 || no_empty_before {
@@ -69,7 +78,11 @@ pub fn get_previous_id(state: WorkspaceState, no_empty_before: bool) -> i32 {
 /// 1) Returns the workspace id after the last occupied workspace
 /// 2) Otherwise, since there are workspaces after on the same monitor, select the one after
 pub fn get_next_id(state: WorkspaceState, no_empty_after: bool) -> i32 {
-    let WorkspaceState { current_id, monitor_ids, occupied_ids } = state;
+    let WorkspaceState {
+        current_id,
+        monitor_ids,
+        occupied_ids,
+    } = state;
 
     if monitor_ids[monitor_ids.len() - 1] == current_id {
         if occupied_ids[occupied_ids.len() - 1] == i32::MAX || no_empty_after {
@@ -84,9 +97,18 @@ pub fn get_next_id(state: WorkspaceState, no_empty_after: bool) -> i32 {
 
 pub fn get_id() -> i32 {
     let state = WorkspaceState::new();
-    let Cli { previous, no_empty_before, no_empty_after, .. } = Cli::parse();
+    let Cli {
+        previous,
+        no_empty_before,
+        no_empty_after,
+        ..
+    } = Cli::parse();
 
     state.log();
 
-    if previous { get_previous_id(state, no_empty_before) } else { get_next_id(state, no_empty_after) }
+    if previous {
+        get_previous_id(state, no_empty_before)
+    } else {
+        get_next_id(state, no_empty_after)
+    }
 }
