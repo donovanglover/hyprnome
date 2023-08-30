f69be8f3a96a0b1b28beba0c4ee138830f577b80
Donovan Glover <donovan@dglover.co>
2023-08-29 20:20:40 -0400

    feat: Finalize get_previous_id() logic

--- src/lib.rs
+++ src/lib.rs
@@ -22,18 +22,22 @@ pub struct WorkspaceState {
     pub occupied_ids: Vec<i32>
 }
 
-/// Gets the id
-pub fn get_id(state: WorkspaceState) -> i32 {
+/// Gets the previous workspace on a monitor, or try to choose a new left-most empty workspace
+pub fn get_previous_id(state: WorkspaceState) -> i32 {
     let WorkspaceState { current_id, monitor_ids, occupied_ids } = state;
-    // let current_id = 500
-    // let occupied_ids = [495, 496, 498, 500, 502, 504, 505]
-    // let monitor_ids = [496, 500, 504]
-    // if current_id is_not_first_in(monitor_ids) -> RelativeMonitor -1
-    // else new_workspace(first_id_of(occupied_ids) - 1)
+
+    // If the current workspace is the first workspace on the monitor
     if monitor_ids[0] == current_id {
-        let id = occupied_ids[0] - 1;
+        // Return the workspace id before the first occupied workspace
+        //
+        // Return workspace 1 if there's no workspace before to choose from
+        //
+        // This basically selects an empty workspace in the direction we want
+        return if occupied_ids[0] == 1 { 1 } else { occupied_ids[0] - 1 };
     }
-    0
+
+    // Otherwise, since there are workspaces before on the same monitor, select the one before.
+    monitor_ids[monitor_ids.iter().position(|&x| x == current_id).unwrap() - 1]
 }
 
 pub fn get_workspace_state() -> WorkspaceState {
