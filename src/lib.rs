41967edf03fa91d73bdce1ac87ff27381157c291
Donovan Glover <donovan@dglover.co>
2024-01-08 17:26:30 -0500

    refactor: Make functions part of implementation
    Makes it possible to access the fields directly with `self` instead of
    having to pass things around.

--- src/lib.rs
+++ src/lib.rs
@@ -54,79 +54,67 @@ impl WorkspaceState {
         log(&format!("Monitor IDs:\t{:?}", self.monitor_ids));
         log(&format!("Occupied IDs:\t{:?}", self.occupied_ids));
     }
-}
 
-/// Gets the previous workspace on a monitor, or try to choose the next left-most empty workspace
-///
-/// 1) If we're the first workspace on a monitor:
-///     1.1) If we're at the lowest possible id 1 or the user doesn't want empty workspaces, return the current id
-///     1.2) Otherwise, return the first unoccupied workspace before the current id
-///         1.2.1) If all other workspaces before are occupied, return the current id instead
-/// 2) Otherwise, since there are workspaces before on the same monitor, select the one before.
-pub fn get_previous_id(state: WorkspaceState, no_empty_before: bool) -> i32 {
-    let WorkspaceState {
-        current_id,
-        monitor_ids,
-        occupied_ids,
-    } = state;
-
-    if monitor_ids[0] == current_id {
-        if monitor_ids[0] == 1 || no_empty_before {
-            current_id
-        } else {
-            let mut i = current_id - 1;
-
-            while i > 0 {
-                if !occupied_ids.contains(&i) {
-                    return i;
-                }
-
-                i -= 1;
-            }
-
-            current_id
-        }
-    } else {
-        monitor_ids[monitor_ids.iter().position(|&x| x == current_id).unwrap() - 1]
-    }
-}
-
-/// Gets the next workspace on a monitor, or try to choose the next right-most empty workspace
-///
-/// 1) If we're the last workspace on a monitor:
-///     1.1) If we're at the MAX or the user doesn't want empty workspaces, return the current id
-///     1.2) Otherwise, return the first unoccupied workspace after the current id
-///         1.2.1) If all other workspaces after are occupied, return the current id instead
-/// 2) Otherwise, since there are workspaces after on the same monitor, select the one after.
-pub fn get_next_id(state: WorkspaceState, no_empty_after: bool) -> i32 {
-    let WorkspaceState {
-        current_id,
-        monitor_ids,
-        occupied_ids,
-    } = state;
-
-    if monitor_ids[monitor_ids.len() - 1] == current_id {
-        if monitor_ids[monitor_ids.len() - 1] == i32::MAX || no_empty_after {
-            current_id
-        } else {
-            let mut i = current_id + 1;
-
-            while i < i32::MAX {
-                if !occupied_ids.contains(&i) {
-                    return i;
-                }
-
-                i += 1;
-            }
-
-            if !occupied_ids.contains(&i) {
-                i
+    /// Gets the previous workspace on a monitor, or try to choose the next left-most empty workspace
+    ///
+    /// 1) If we're the first workspace on a monitor:
+    ///     1.1) If we're at the lowest possible id 1 or the user doesn't want empty workspaces, return the current id
+    ///     1.2) Otherwise, return the first unoccupied workspace before the current id
+    ///         1.2.1) If all other workspaces before are occupied, return the current id instead
+    /// 2) Otherwise, since there are workspaces before on the same monitor, select the one before.
+    pub fn get_previous_id(&self, no_empty_before: bool) -> i32 {
+        if self.monitor_ids[0] == self.current_id {
+            if self.monitor_ids[0] == 1 || no_empty_before {
+                self.current_id
             } else {
-                current_id
+                let mut i = self.current_id - 1;
+
+                while i > 0 {
+                    if !self.occupied_ids.contains(&i) {
+                        return i;
+                    }
+
+                    i -= 1;
+                }
+
+                self.current_id
             }
+        } else {
+            self.monitor_ids[self.monitor_ids.iter().position(|&x| x == self.current_id).unwrap() - 1]
         }
-    } else {
-        monitor_ids[monitor_ids.iter().position(|&x| x == current_id).unwrap() + 1]
+    }
+
+    /// Gets the next workspace on a monitor, or try to choose the next right-most empty workspace
+    ///
+    /// 1) If we're the last workspace on a monitor:
+    ///     1.1) If we're at the MAX or the user doesn't want empty workspaces, return the current id
+    ///     1.2) Otherwise, return the first unoccupied workspace after the current id
+    ///         1.2.1) If all other workspaces after are occupied, return the current id instead
+    /// 2) Otherwise, since there are workspaces after on the same monitor, select the one after.
+    pub fn get_next_id(&self, no_empty_after: bool) -> i32 {
+        if self.monitor_ids[self.monitor_ids.len() - 1] == self.current_id {
+            if self.monitor_ids[self.monitor_ids.len() - 1] == i32::MAX || no_empty_after {
+                self.current_id
+            } else {
+                let mut i = self.current_id + 1;
+
+                while i < i32::MAX {
+                    if !self.occupied_ids.contains(&i) {
+                        return i;
+                    }
+
+                    i += 1;
+                }
+
+                if !self.occupied_ids.contains(&i) {
+                    i
+                } else {
+                    self.current_id
+                }
+            }
+        } else {
+            self.monitor_ids[self.monitor_ids.iter().position(|&x| x == self.current_id).unwrap() + 1]
+        }
     }
 }
 
