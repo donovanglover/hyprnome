67e6fbfade50b65ba7b191c2eda4a400aa9bf59a
Donovan Glover <donovan@dglover.co>
2024-01-08 20:14:56 -0500

    refactor: Rename workspace_state to state

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ mod hyprland;
 /// Specific features are abstracted into lib to make things testable.
 fn main() {
     let (_move, keep_special, previous, no_empty, no_empty_before, no_empty_after) = cli::get_options();
-    let workspace_state = hyprland::get_state();
+    let state = hyprland::get_state();
 
-    cli::log(&format!("{}", workspace_state));
+    cli::log(&format!("{}", state));
 
     let id = if previous {
-        workspace_state.get_previous_id(no_empty || no_empty_before)
+        state.get_previous_id(no_empty || no_empty_before)
     } else {
-        workspace_state.get_next_id(no_empty || no_empty_after)
+        state.get_next_id(no_empty || no_empty_after)
     };
 
     cli::log(&format!("Dispatched ID:\t{id}"));
