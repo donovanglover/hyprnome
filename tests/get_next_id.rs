3033ac0b0f0e317ead4fb3514426a1ac53e14f9b
Donovan Glover <donovan@dglover.co>
2024-01-08 23:09:20 -0500

    chore: Make tests work with new struct fields

--- tests/get_next_id.rs
+++ tests/get_next_id.rs
@@ -9,14 +9,16 @@ fn only_workspace() {
     let occupied_ids = [500].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(false), 501, "should return next workspace on monitor when only workspace");
+    assert_eq!(state.get_next_id(), 501, "should return next workspace on monitor when only workspace");
 
     let current_id = 498;
     let monitor_ids = [498].to_vec();
     let occupied_ids = [498].to_vec();
-    let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
+    let mut state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(true), 498, "should return the same workspace if only workspace and no_empty_after");
+    state.set_no_empty_after(true);
+
+    assert_eq!(state.get_next_id(), 498, "should return the same workspace if only workspace and no_empty_after");
 }
 
 #[test]
@@ -26,14 +28,14 @@ fn next_workspace_on_monitor() {
     let occupied_ids = [500, 502].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(false), 502, "should return next workspace on monitor");
+    assert_eq!(state.get_next_id(), 502, "should return next workspace on monitor");
 
     let current_id = 500;
     let monitor_ids = [500, 504].to_vec();
     let occupied_ids = [500, 502, 504].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(false), 504, "should return next workspace on monitor with occupied workspaces in-between on other monitors");
+    assert_eq!(state.get_next_id(), 504, "should return next workspace on monitor with occupied workspaces in-between on other monitors");
 }
 
 #[test]
@@ -43,14 +45,14 @@ fn next_empty() {
     let occupied_ids = [500, 502].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(false), 503, "should return next empty workspace if last workspace on monitor");
+    assert_eq!(state.get_next_id(), 503, "should return next empty workspace if last workspace on monitor");
 
     let current_id = 502;
     let monitor_ids = [500, 502].to_vec();
     let occupied_ids = [500, 502, 503, 504].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(false), 505, "should return next empty workspace if last workspace on monitor with occupied workspaces on other monitors");
+    assert_eq!(state.get_next_id(), 505, "should return next empty workspace if last workspace on monitor with occupied workspaces on other monitors");
 }
 
 #[test]
@@ -58,16 +60,20 @@ fn no_empty_after() {
     let current_id = 502;
     let monitor_ids = [500, 502].to_vec();
     let occupied_ids = [500, 502].to_vec();
-    let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
+    let mut state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(true), 502, "should return the same workspace if last workspace on monitor");
+    state.set_no_empty_after(true);
+
+    assert_eq!(state.get_next_id(), 502, "should return the same workspace if last workspace on monitor");
 
     let current_id = 502;
     let monitor_ids = [500, 502].to_vec();
     let occupied_ids = [500, 502, 503, 504].to_vec();
-    let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
+    let mut state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(true), 502, "should return the same workspace if last workspace on monitor with occupied workspaces on other monitors");
+    state.set_no_empty_after(true);
+
+    assert_eq!(state.get_next_id(), 502, "should return the same workspace if last workspace on monitor with occupied workspaces on other monitors");
 }
 
 #[test]
@@ -77,14 +83,14 @@ fn out_of_bounds() {
     let occupied_ids = [500, i32::MAX].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(false), i32::MAX, "should return the same workspace if last workspace is i32::MAX");
+    assert_eq!(state.get_next_id(), i32::MAX, "should return the same workspace if last workspace is i32::MAX");
 
     let current_id = i32::MAX - 2;
     let monitor_ids = [i32::MAX - 2].to_vec();
     let occupied_ids = [i32::MAX - 2, i32::MAX - 1, i32::MAX].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(false), i32::MAX - 2, "should return the current workspace if all next workspaces are occupied");
+    assert_eq!(state.get_next_id(), i32::MAX - 2, "should return the current workspace if all next workspaces are occupied");
 }
 
 #[test]
@@ -94,14 +100,14 @@ fn fill_the_gaps() {
     let occupied_ids = [3, 4, 6].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(false), 5, "should return workspace 5 if the occupied workspaces are [3, 4, 6], the monitor workspaces are [3, 4], and the current workspace is 4");
+    assert_eq!(state.get_next_id(), 5, "should return workspace 5 if the occupied workspaces are [3, 4, 6], the monitor workspaces are [3, 4], and the current workspace is 4");
 
     let current_id = 5;
     let monitor_ids = [4, 5].to_vec();
     let occupied_ids = [4, 5, 6, 7, 9].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(false), 8, "should return workspace 8 if the occupied workspaces are [4, 5, 6, 7, 9], the monitor workspaces are [4, 5], and the current workspace is 5");
+    assert_eq!(state.get_next_id(), 8, "should return workspace 8 if the occupied workspaces are [4, 5, 6, 7, 9], the monitor workspaces are [4, 5], and the current workspace is 5");
 }
 
 #[test]
@@ -111,12 +117,12 @@ fn returns_the_last_id() {
     let occupied_ids = [i32::MAX - 1].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(false), i32::MAX, "should return i32::MAX if it's unoccupied");
+    assert_eq!(state.get_next_id(), i32::MAX, "should return i32::MAX if it's unoccupied");
 
     let current_id = i32::MAX - 2;
     let monitor_ids = [i32::MAX - 2].to_vec();
     let occupied_ids = [i32::MAX - 2, i32::MAX - 1].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_next_id(false), i32::MAX, "should return i32::MAX if it's unoccupied and there are in-between workspaces on other monitors");
+    assert_eq!(state.get_next_id(), i32::MAX, "should return i32::MAX if it's unoccupied and there are in-between workspaces on other monitors");
 }
--- tests/get_previous_id.rs
+++ tests/get_previous_id.rs
@@ -9,14 +9,16 @@ fn only_workspace() {
     let occupied_ids = [500].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(false), 499, "should return previous workspace on monitor when only workspace");
+    assert_eq!(state.get_previous_id(), 499, "should return previous workspace on monitor when only workspace");
 
     let current_id = 500;
     let monitor_ids = [500].to_vec();
     let occupied_ids = [500].to_vec();
-    let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
+    let mut state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(true), 500, "should return the same workspace if only workspace and no_empty_before");
+    state.set_no_empty_before(true);
+
+    assert_eq!(state.get_previous_id(), 500, "should return the same workspace if only workspace and no_empty_before");
 }
 
 #[test]
@@ -26,14 +28,14 @@ fn previous_workspace_on_monitor() {
     let occupied_ids = [500, 502].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(false), 500, "should return previous workspace on monitor");
+    assert_eq!(state.get_previous_id(), 500, "should return previous workspace on monitor");
 
     let current_id = 504;
     let monitor_ids = [500, 504].to_vec();
     let occupied_ids = [500, 502, 504].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(false), 500, "should return previous workspace on monitor with occupied workspaces in-between on other monitors");
+    assert_eq!(state.get_previous_id(), 500, "should return previous workspace on monitor with occupied workspaces in-between on other monitors");
 }
 
 #[test]
@@ -43,14 +45,14 @@ fn previous_empty() {
     let occupied_ids = [500, 501].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(false), 499, "should return previous empty workspace if first workspace on monitor");
+    assert_eq!(state.get_previous_id(), 499, "should return previous empty workspace if first workspace on monitor");
 
     let current_id = 500;
     let monitor_ids = [500, 501].to_vec();
     let occupied_ids = [499, 500, 501].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(false), 498, "should return previous empty workspace if first workspace on monitor with occupied workspaces on other monitors");
+    assert_eq!(state.get_previous_id(), 498, "should return previous empty workspace if first workspace on monitor with occupied workspaces on other monitors");
 }
 
 #[test]
@@ -58,16 +60,20 @@ fn no_empty_before() {
     let current_id = 500;
     let monitor_ids = [500, 502].to_vec();
     let occupied_ids = [500, 502].to_vec();
-    let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
+    let mut state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(true), 500, "should return the same workspace if first workspace on monitor");
+    state.set_no_empty_before(true);
+
+    assert_eq!(state.get_previous_id(), 500, "should return the same workspace if first workspace on monitor");
 
     let current_id = 500;
     let monitor_ids = [500, 502].to_vec();
     let occupied_ids = [498, 499, 500, 502].to_vec();
-    let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
+    let mut state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(true), 500, "should return the same workspace if first workspace on monitor with occupied workspaces on other monitors");
+    state.set_no_empty_before(true);
+
+    assert_eq!(state.get_previous_id(), 500, "should return the same workspace if first workspace on monitor with occupied workspaces on other monitors");
 }
 
 #[test]
@@ -77,14 +83,14 @@ fn out_of_bounds() {
     let occupied_ids = [1, 2].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(false), 1, "should return the same workspace if first workspace is 1");
+    assert_eq!(state.get_previous_id(), 1, "should return the same workspace if first workspace is 1");
 
     let current_id = 3;
     let monitor_ids = [3, 4].to_vec();
     let occupied_ids = [1, 2, 3, 4].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(false), 3, "should return the current workspace if all previous workspaces are occupied");
+    assert_eq!(state.get_previous_id(), 3, "should return the current workspace if all previous workspaces are occupied");
 }
 
 #[test]
@@ -94,14 +100,14 @@ fn fill_the_gaps() {
     let occupied_ids = [1, 3, 4].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(false), 2, "should return workspace 2 if the occupied workspaces are [1, 3, 4], the monitor workspaces are [3, 4], and the current workspace is 3");
+    assert_eq!(state.get_previous_id(), 2, "should return workspace 2 if the occupied workspaces are [1, 3, 4], the monitor workspaces are [3, 4], and the current workspace is 3");
 
     let current_id = 4;
     let monitor_ids = [4, 5].to_vec();
     let occupied_ids = [1, 3, 4, 5].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(false), 2, "should return workspace 2 if the occupied workspaces are [1, 3, 4, 5], the monitor workspaces are [4, 5], and the current workspace is 4");
+    assert_eq!(state.get_previous_id(), 2, "should return workspace 2 if the occupied workspaces are [1, 3, 4, 5], the monitor workspaces are [4, 5], and the current workspace is 4");
 }
 
 #[test]
@@ -111,12 +117,12 @@ fn returns_the_first_id() {
     let occupied_ids = [2, 3].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(false), 1, "should return 1 if it's unoccupied");
+    assert_eq!(state.get_previous_id(), 1, "should return 1 if it's unoccupied");
 
     let current_id = 4;
     let monitor_ids = [4, 5].to_vec();
     let occupied_ids = [2, 3, 4, 5].to_vec();
     let state = WorkspaceState::new(current_id, monitor_ids, occupied_ids);
 
-    assert_eq!(state.get_previous_id(false), 1, "should return 1 if it's unoccupied and there are in-between workspaces on other monitors");
+    assert_eq!(state.get_previous_id(), 1, "should return 1 if it's unoccupied and there are in-between workspaces on other monitors");
 }
